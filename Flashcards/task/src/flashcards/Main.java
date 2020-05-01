630c7cc259f3118f396840e97bdcb6dfd5cf51de
Matt <beefy1875@protonmail.com>
2020-05-01 11:26:39 +0800

    Initial commit

---
+++ Flashcards/task/src/flashcards/Main.java
@@ -0,0 +1,12 @@
+package flashcards;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Card: ");
+        System.out.println("purchase");
+        System.out.println("Definition: ");
+        System.out.println("buy");
+
+    }
+}
